649f1fe65332ad7bafd25685cfe1ee34bf5a73c0
Ademola Ibukun <hybiekay2@gmail.com>
2025-05-28 10:59:57 +0100

    Wotkin

--- lib/main.dart
+++ lib/main.dart
@@ -31,24 +31,77 @@ class GameScreenState extends State<GameScreen> {
   List boards = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   int player = 1;
   int computer = 2;
+  String status = '';
 
   runComputer() async {
-    Future.delayed(Duration(microseconds: 500), () {
+    Future.delayed(Duration(milliseconds: 500), () async {
+      if (isWin(player, boards)) {
+        setState(() {
+          boards = List.filled(9, 0);
+        });
+      }
+
+      if (boards.every((b) => b != 0)) {
+        setState(() {
+          boards = List.filled(9, 0);
+        });
+      }
       int? blocking;
       int? winning;
       int? normalMove;
-      List boardsFrom = List.from(boards);
-      for (int i = 0; i < boardsFrom.length; i++) {
+
+      for (int i = 0; i < boards.length; i++) {
+        List boardsFrom = List.from(boards);
         if (boardsFrom[i] != 0) {
           continue;
         }
         boardsFrom[i] = computer;
-        if (isWin(computer, boardsFrom)) {}
+        if (isWin(computer, boardsFrom)) {
+          winning = i;
+        }
+
+        boardsFrom[i] = player;
+
+        if (isWin(player, boardsFrom)) {
+          blocking = i;
+        }
+        normalMove = i;
+      } // looping
+
+      var mainMove = winning ?? blocking ?? normalMove;
+
+      await Future.delayed(Duration(milliseconds: 200), () {
+        setState(() {
+          boards[mainMove!] = computer;
+        });
+      });
+
+      if (isWin(computer, boards)) {
+        setState(() {
+          boards = List.filled(9, 0);
+        });
       }
     });
   }
 
-  isWin(int user, List boards) {}
+  bool isWin(int user, List boards) {
+    //012
+    //345
+    //678
+    //036
+    //147
+    //258
+    // 048
+    //246
+    return boards[0] == user && boards[1] == user && boards[2] == user ||
+        boards[3] == user && boards[4] == user && boards[5] == user ||
+        boards[6] == user && boards[7] == user && boards[8] == user ||
+        boards[0] == user && boards[3] == user && boards[6] == user ||
+        boards[1] == user && boards[4] == user && boards[7] == user ||
+        boards[2] == user && boards[5] == user && boards[8] == user ||
+        boards[0] == user && boards[4] == user && boards[4] == user ||
+        boards[2] == user && boards[4] == user && boards[6] == user;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -97,6 +150,16 @@ class GameScreenState extends State<GameScreen> {
                 for (int i = 0; i < boards.length; i++)
                   GestureDetector(
                     onTap: () {
+                      if (boards[i] != 0) {
+                        setState(() {
+                          status =
+                              boards[i] == 1
+                                  ? "You already played here"
+                                  : "Computer has played there";
+                        });
+
+                        return;
+                      }
                       setState(() {
                         boards[i] = player;
                       });
@@ -112,11 +175,27 @@ class GameScreenState extends State<GameScreen> {
                                 : Colors.amber,
                         borderRadius: BorderRadius.circular(10),
                       ),
+                      child: Center(
+                        child: Text(
+                          boards[i] == player
+                              ? "O"
+                              : boards[i] == computer
+                              ? "X"
+                              : "",
+
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
                     ),
                   ),
               ],
             ),
           ),
+
+          Center(child: Text(status, style: TextStyle(fontSize: 17))),
         ],
       ),
     );
